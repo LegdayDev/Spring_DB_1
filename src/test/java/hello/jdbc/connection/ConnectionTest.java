e7d49965c6c97b1c70b8531134d2092c82a8652a
Choi Jae Young <chlwodud0327@naver.com>
2024-01-08 19:00:20 +0900

    Feat: DataSource 사용예제 - HikariCP 오픈소스 사용

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -32,11 +33,25 @@ public class ConnectionTest {
 
     }
 
+    @Test
+    public void dataSourceConnectionPool() throws Exception {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("My Connection Pool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
 
-        log.info("connection={}, class={}",con1,con1.getClass());
-        log.info("connection={}, class={}",con2,con2.getClass());
+        log.info("connection={}, class={}",con1 ,con1 .getClass());
+        log.info("connection={}, class={}",con2 ,con2 .getClass());
     }
 }
